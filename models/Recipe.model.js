b55df6cf5a7b02f2b5ef41f7d0ca724f55019b20
Erick-34 <erickoliveira2060@gmail.com>
2020-07-02 17:39:03 -0300

    Done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  name: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: Array,
+  },
+  cuisine: {
+    type: String,
+    require: true,
+  },
+  dishType: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: {
+    type: String,
+  },
+  reated: {
+    type: Date,
+  },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
